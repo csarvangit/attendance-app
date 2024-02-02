9aab779483a212d302c705ea15cabc8cf52d06a9
mrlsiva <54312287+mrlsiva@users.noreply.github.com>
2024-02-02 19:06:16 +0530

    acdjks

--- resources/views/admin/chitfund/print-invoice.blade.php
+++ resources/views/admin/chitfund/print-invoice.blade.php
@@ -69,7 +69,7 @@ use Carbon\Carbon;
 .printInvoice th,
 .printInvoice tr,
 .printInvoice table {
-	border-top: 1px solid black;
+	border-bottom: 1px solid black;
 	border-collapse: collapse;
 }
 .token {
@@ -88,7 +88,7 @@ use Carbon\Carbon;
 		<p class="centered fw-bold mt-0">Contact: 99943 33605</p> 
 		<p class="centered fw-bold mt-2 mb-0">VASANTHAM SIRU SEMIPPU THITTAM</p>
 		<p class="centered fw-bold mt-2">Token Number</p>
-		<p class="centered fw-bold mt-2 token">#{{ $user[0]->user_id }}</p>
+		<p class="centered fw-bold mt-2 mb-3 token">#{{ $user[0]->user_id }}</p>
 			
 		<!-- <p class="centered  mt-2">Invoice No: <span class="invoice-no"> #{{$billId}}</span></p>
 		<p class="centered  mt-2">Date: <span class="invoice-date">{{ Carbon::now()->format('Y-m-d H:i:s') }}</span></p> -->
@@ -99,7 +99,7 @@ use Carbon\Carbon;
 		<thead>
 			<tr>
 				<th>Invoice No: <span class="invoice-no"> #{{$billId}}</span></th>
-				<th>Date: <span class="invoice-date">{{ Carbon::now()->format('Y-m-d H:i:s') }}</span></th>
+				<th style="    text-align: right;">Date: <span class="invoice-date">{{ Carbon::now()->format('Y-m-d H:i:s') }}</span></th>
 			</tr>
 		</thead>
 		<tbody>
