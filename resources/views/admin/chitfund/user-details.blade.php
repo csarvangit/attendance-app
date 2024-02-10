7343bb9d0563e68fd87060801bc023e43798668d
siva <pmrlsivas@gmail.com>
2024-02-10 09:55:25 +0530

    csdv

--- resources/views/admin/chitfund/user-details.blade.php
+++ resources/views/admin/chitfund/user-details.blade.php
@@ -81,7 +81,7 @@ use Carbon\Carbon;
 															$target = "_self";
 															if( $u->due_status == 1 ){		
 																$date = Carbon::parse($u->due_date_paid )->format('d-m-Y');
-																$whatsapp_message = urlencode("Dear $u->user_name, you have paid Rs:$u->plan_amount for the date $date of vasantham chit fund $u->plan_name. Thank you. - Vasantham Home Appliances - Siru Semippu Thittam.");
+																$whatsapp_message = urlencode("Dearsss $u->user_name, you have paid Rs:$u->plan_amount for the date $date of vasantham chit fund $u->plan_name. Thank you. - Vasantham Home Appliances - Siru Semippu Thittam.");
 																$whatsapp_url = "https://wa.me/$u->mobile_no?text=$whatsapp_message";
 																$target = "_blank";
 															}
